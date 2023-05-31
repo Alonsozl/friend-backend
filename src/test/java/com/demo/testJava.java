46e866425a9639b577989933d8dba1cf68533b57
Alonso <Alonso@qq.com>
2023-05-31 16:24:40 +0800

    init

---
+++ src/test/java/com/demo/testJava.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class testJava {
+
+    @Test
+    public void test(){
+        System.out.println("test....");
+    }
+}
